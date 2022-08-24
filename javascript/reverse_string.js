18f763d8be7544d1a3fc0625cf2550a955f217e9
Charles <c_onyemem@live.com>
2022-08-24 13:30:26 -0700

    Completed Task

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,11 @@
 function reverseString(str) {
   // type your code here
+
+    let newArr = Array.from(str)
+    let revArr = newArr.reverse()
+    
+    return revArr.join("")
+  
 }
 
 if (require.main === module) {
@@ -16,4 +22,13 @@ if (require.main === module) {
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+// 1. newArr <- [str]
+// 2. Reverse newArr character order
+// 3. Convert to string
+// 4. return result
+
 // And a written explanation of your solution
+// 1. Take passed in string then convert it to an array and assign it to a variable.
+// 2. Call the reverse method on the array 
+// 3. Convert back to string
+// 4. Return the the new array variable
